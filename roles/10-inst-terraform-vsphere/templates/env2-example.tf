60ef25552278e2b66d95c41851daa64202c434b4
ansible <ansible@pod01-vm2-tf.timcloud.local>
2022-11-21 00:01:28 +0000

    Внесены изменения в права ззапуска, чтоб запускать скрипт из под пользователя ansibleÑ. Исправлены скрÐконфигурации шаблонов ВМ. Устранены ошибки установки terraform и packerÑ.

--- roles/10-inst-terraform-vsphere/templates/env2-example.tf
+++ roles/10-inst-terraform-vsphere/templates/env2-example.tf
@@ -7,8 +7,8 @@ variable "env2-group1" {
     default = {
 
       #ENV env2-group1
-      pod01-vm1-app = {name = "pod01-vm1-app", cpu_count = 1, memory = 1024, datastore = "datastore2", disk_size = 16, network="VM Network", template = "tf-templ-debian11", guest_id="debian10_64Guest"}},
-      pod01-vm2-web = {name = "pod01-vm2-web", cpu_count = 1, memory = 1024, datastore = "datastore2", disk_size = 16, network="VM Network", template = "tf-templ-debian11", guest_id="debian10_64Guest"}},
+      pod01-vm1-app = {name = "pod01-vm1-app", cpu_count = 1, memory = 1024, datastore = "datastore3", disk_size = 32, network="VM Network", template = "tf-templ-debian11", guest_id="debian10_64Guest"},
+      pod01-vm2-web = {name = "pod01-vm2-web", cpu_count = 1, memory = 1024, datastore = "datastore3", disk_size = 32, network="VM Network", template = "tf-templ-debian11", guest_id="debian10_64Guest"},
 
     }
   }
@@ -18,8 +18,8 @@ variable "env2-group2" {
     default = {
 
       #ENV env2-group2
-      pod02-vm1-app = {name = "pod01-vm1-app", cpu_count = 1, memory = 1024, datastore = "datastore3", disk_size = 16, network="VM Network", template = "tf-templ-ubuntu14", guest_id="ubuntu64Guest"},
-      pod02-vm2-web = {name = "pod02-vm2-web", cpu_count = 1, memory = 1024, datastore = "datastore3", disk_size = 16, network="VM Network", template = "tf-templ-ubuntu14", guest_id="ubuntu64Guest"},
+      pod02-vm1-app = {name = "pod02-vm1-app", cpu_count = 1, memory = 1024, datastore = "datastore2", disk_size = 16, network="VM Network", template = "tf-templ-ubuntu14", guest_id="ubuntu64Guest"},
+      pod02-vm2-web = {name = "pod02-vm2-web", cpu_count = 1, memory = 1024, datastore = "datastore2", disk_size = 16, network="VM Network", template = "tf-templ-ubuntu14", guest_id="ubuntu64Guest"},
 
     }
   }
