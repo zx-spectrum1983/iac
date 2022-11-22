c4c201bca585b1c32d73b32ac201064d6dd2505a
ansible <ansible@pod01-vm1-iac1.vsphere.local>
2022-11-22 22:36:02 +0000

    Hotfix: исправил проблему hosts-tf.py, когда terraform установлен, но план еще пустой и отсутствует output hosts.

--- inventory/hosts-tf.py
+++ inventory/hosts-tf.py
@@ -10,9 +10,13 @@ isExist = os.path.exists(tfdir)
 if isExist:
    cwd = os.getcwd()
    os.chdir(tfdir)
-   proc = subprocess.run(["terraform", "output", "-json", "hosts"], stdout=subprocess.PIPE)
-   jsonHosts = json.loads(proc.stdout.decode('utf-8'))
-   print(json.dumps(jsonHosts,  indent=4))
+   proc = subprocess.run(["terraform", "output", "-json"], stdout=subprocess.PIPE)
+   if proc.stdout.decode('utf-8').strip(" \r\n") == "{}":
+      print("{ }")
+   else:
+      proc = subprocess.run(["terraform", "output", "-json", "hosts"], stdout=subprocess.PIPE)
+      jsonHosts = json.loads(proc.stdout.decode('utf-8'))
+      print(json.dumps(jsonHosts,  indent=4))
    os.chdir(cwd)
 else:
    print("{ }")
