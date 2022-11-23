26ad40d3fcf57fbddc19dc0c28fd73801a1cc26e
ansible <ansible@pod01-vm1-iac1.vsphere.local>
2022-11-23 11:54:50 +0000

    Переделал механизм хранения переменных с yaml на json и добавил скрипт в inventory. Теперь указывать явно файл с переменными не нужно: система автоматически ищет секреты в vault, если там нет, тогда в /home/ansible/init-iac.json

---
+++ inventory/hosts-vars.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import os
+import json
+import requests
+
+varsJson = '{"all":{"vars":{}}}'
+data = json.loads(varsJson)
+
+tokenfile = "/home/ansible/vault_ansible_token"
+configfile = "/home/ansible/init-iac.json"
+isExistT = os.path.exists(tokenfile)
+isExistC = os.path.exists(configfile)
+try:
+   isOnline = requests.head('http://127.0.0.1:8200/v1/sys/health', verify=False, timeout=1)
+except:
+   isOnline = False
+   pass
+
+if isExistT and isOnline:
+   ft = open(tokenfile)
+   token = ft.read()
+   url = 'http://127.0.0.1:8200/v1/ansible/config'
+   headers = {'X-Vault-Token': token}
+   response = requests.get(url, headers=headers)
+   vaultJson = json.loads(response.text)
+   data['all']['vars'] = vaultJson['data']
+   print(json.dumps(data, indent=4))
+   ft.close()
+elif isExistC:
+   ft = open(configfile)
+   configJson = json.load(ft)
+   data['all']['vars'] = configJson
+   print(json.dumps(data, indent=4))
+   ft.close()
+else:
+   print(json.dumps(data, indent=4))
+
