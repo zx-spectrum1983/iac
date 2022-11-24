dd5a876061423d08fd72168681c972fec55a8593
ansible <ansible@pod01-vm1-iac1.vsphere.local>
2022-11-24 16:32:44 +0000

    Добавлена настройка Vault и прозрачный импорт секретов из Vault в ansible с помощью скрипта.

--- inventory/hosts-vars.py
+++ inventory/hosts-vars.py
@@ -4,10 +4,40 @@ import os
 import json
 import requests
 
+def get_config():
+   global isExistT
+   if isExistT and isOnline:
+      ft = open(tokenfile)
+      token = ft.read()
+      url = 'http://127.0.0.1:8200/v1/ansible/config'
+      headers = {'X-Vault-Token': token}
+      response = requests.get(url, headers=headers)
+      vaultJson = json.loads(response.text)
+      ft.close()
+      try:
+         data['all']['vars'] = vaultJson['data']
+      except KeyError:
+         isExistT = False
+         get_config()
+      else:
+         data['all']['vars'] = vaultJson['data']
+         print(json.dumps(data, indent=4))
+         return
+   elif isExistC:
+      ft = open(configfile)
+      configJson = json.load(ft)
+      data['all']['vars'] = configJson
+      print(json.dumps(data, indent=4))
+      ft.close()
+   else:
+      print(json.dumps(data, indent=4))
+   return
+
+
 varsJson = '{"all":{"vars":{}}}'
 data = json.loads(varsJson)
 
-tokenfile = "/home/ansible/vault_ansible_token"
+tokenfile = "/home/ansible/.vault_ansible_token"
 configfile = "/home/ansible/init-iac.json"
 isExistT = os.path.exists(tokenfile)
 isExistC = os.path.exists(configfile)
@@ -16,23 +46,5 @@ try:
 except:
    isOnline = False
    pass
-
-if isExistT and isOnline:
-   ft = open(tokenfile)
-   token = ft.read()
-   url = 'http://127.0.0.1:8200/v1/ansible/config'
-   headers = {'X-Vault-Token': token}
-   response = requests.get(url, headers=headers)
-   vaultJson = json.loads(response.text)
-   data['all']['vars'] = vaultJson['data']
-   print(json.dumps(data, indent=4))
-   ft.close()
-elif isExistC:
-   ft = open(configfile)
-   configJson = json.load(ft)
-   data['all']['vars'] = configJson
-   print(json.dumps(data, indent=4))
-   ft.close()
-else:
-   print(json.dumps(data, indent=4))
+get_config()
 
